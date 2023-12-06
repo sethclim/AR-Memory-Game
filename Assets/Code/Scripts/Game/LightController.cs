4a9118e38ccbec5270f3d66dd5f5f7bcadd26f75
Lin Cui <39138182+cuilin127@users.noreply.github.com>
2023-12-05 20:24:46 -0500

    All bulb has a pinao key as sound

--- Assets/Code/Scripts/Game/LightController.cs
+++ Assets/Code/Scripts/Game/LightController.cs
@@ -11,6 +11,7 @@ namespace ARMG
         [SerializeField] Color m_lightColor;
         [SerializeField] MeshRenderer m_meshRenderer;
         [SerializeField] Light m_light;
+        [SerializeField] AudioSource m_source;
 
         Color m_tranparentColor = new Color { r = 1.0f, g = 1.0f, b = 1.0f, a = 0.5f };
 
@@ -23,6 +24,9 @@ namespace ARMG
             m_meshRenderer.material.color = onOff ? color : m_tranparentColor;
             m_light.color = color;
             m_light.enabled = onOff;
+            if (onOff) {
+                m_source.Play();
+            }
         }
 
 #if UNITY_EDITOR
